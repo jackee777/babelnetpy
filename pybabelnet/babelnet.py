8cce744e5072311977dd02fdc97ca1e2f95025a3
jackee777 <c5618168@aoyama.jp>
2018-12-04 16:23:29 +0900

    add parameters in the search

--- pybabelnet/babelnet.py
+++ pybabelnet/babelnet.py
@@ -9,39 +9,77 @@ class BabelNet(object):
         self.synsets = []
         self.senses = []
         #self.lemma = ""
-        #self.lang = ""
+        self.lang = None
         self.key = key_path
-    
-    def getSynset_Ids(self, lemma, lang):
-        synset_url = self.API_PATH + \
-            "getSynsetIds?lemma={0}&searchLang={1}&key={2}"
-        synset_url = synset_url.format(lemma, lang, self.key)
+
+    def make_url(self, **params):
+        synset_url = self.API_PATH
+        synset_url += params["function"]
+        if params.get("lemma"):
+            synset_url += "lemma={0}".format(params["lemma"])
+        if params.get("id"):
+            synset_url += "id={0}".format(params["id"])
+        if params.get("lang"):
+            synset_url += "&searchLang={0}".format(params["lang"])
+        if params.get("targetLang") and params["targetLang"]!=None:
+            if type(params["targetLang"]) == list:
+                assert len(params["targetLang"]) == 2, "targetLang is not more than 3"
+                synset_url += "&targetLang={0}".format(params["targetLang"][0])
+                synset_url += "&targetLang={0}".format(params["targetLang"][1])
+            else:
+                synset_url += "&targetLang={0}".format(params["targetLang"])
+        if params.get("pos") and params["pos"]!=None:
+            if type(params["pos"]) == list:
+                for pos in params["pos"]:
+                    synset_url += "&pos={0}".format(pos)
+            else:
+                synset_url += "&pos={0}".format(params["pos"])
+        if params.get("source") and params["source"]!=None:
+            if type(params["source"]) == list:
+                for source in params["source"]:
+                    synset_url += "&source={0}".format(source)
+            else:
+                synset_url += "&source={0}".format(params["source"])
+
+        synset_url += "&key={0}".format(self.key)
+        return synset_url
+
+    def getSynset_Ids(self, lemma, lang, targetLang=None, pos=None, source=None):
+        self.lang = lang
+        synset_url = self.make_url(function="getSynsetIds?",
+            lemma=lemma, lang=self.lang, pos=pos, source=source)
+        synset_url = synset_url.format(lemma, self.lang, self.key)
         synset_json = urllib.request.urlopen(synset_url).read()
         synset_json = synset_json.decode("utf8")
         synset_ids = [dict2obj(js) for js in json.loads(synset_json)]
         self.synset_ids = synset_ids
         return synset_ids
-    
-    def getSynsets(self, synset_id):
-        synset_url = self.API_PATH + \
-            "getSynset?id={0}&key={1}"
-        synset_url = synset_url.format(synset_id, self.key)
+
+    def getSynsets(self, synset_id, targetLang=None, change_lang=False):
+        if change_lang and targetLang != None:
+            if type(targetLang) == list:
+                self.lang = targetLang[0]
+            else:
+                self.lang = targetLang
+        targetLang = targetLang or self.lang
+        synset_url = self.make_url(function="getSynset?",
+            id=synset_id, targetLang=targetLang)
         synset_json = urllib.request.urlopen(synset_url).read()
         synset_json = synset_json.decode("utf8")
         synsets = [dict2obj(json.loads(synset_json))]
         self.synsets = synsets
         return synsets
-    
+
     def getOutgoingEdges(self, synset_id):
-        synset_url = self.API_PATH + \
-            "getOutgoingEdges?id={0}&key={1}"
+        synset_url = self.make_url(function="getOutgoingEdges?",
+            id=synset_id)
         synset_url = synset_url.format(synset_id, self.key)
         synset_json = urllib.request.urlopen(synset_url).read()
         synset_json = synset_json.decode("utf8")
         edges = [dict2obj(js) for js in json.loads(synset_json)]
         self.edges = edges
         return edges
-    
+
     def getSenses(self, lemma, lang, lemma_type="full", force_compare=True):
         senses = []
         if "full" == lemma_type:
@@ -66,10 +104,10 @@ class BabelNet(object):
                         senses.append(sense)
         self.senses = senses
         return senses
-    
+
     def getSynsetIdsFromResourceID(self, lemma, lang, pos, source, \
         lemma_type="full", force_compare=True):
-        
+
         synset_ids = []
         if "full" == lemma_type:
             for synset in self.synsets:
